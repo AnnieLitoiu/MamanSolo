7674265db99503165c9a427ac061bcb8b70e7aaa
Yuliiasergiivna <kremposka@gmail.com>
2025-11-13 19:26:54 +0100

    petit changement

--- src/Controller/ScoresController.php
+++ src/Controller/ScoresController.php
@@ -15,7 +15,7 @@ class ScoresController extends AbstractController
     // Page "Tableau des scores"
     #[Route('/scores', name: 'api_scores', methods: ['GET'])]
     public function scores(GameDesignStats $stats): JsonResponse
-    {    
+    {
         $scores = $stats->calculerScoresMoyens();
         // Pour l’instant on affiche une page vide/placeholder.
         // On pourra y injecter les vrais scores plus tard.
